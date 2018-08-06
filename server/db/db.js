44392cec0bddd605c54b5095396d3d390c8edd95
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-08-05 23:12:10 -0400

    heroku?

--- server/db/db.js
+++ server/db/db.js
@@ -3,29 +3,12 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const createDB = () => {
-  if (process.env.USER === 'notnull') {
-    const config = require('./config')
-    const db = new Sequelize(
-      config.database,
-      config.username,
-      config.password,
-      config.options
-    )
-    return db
-  } else {
-    const db = new Sequelize(
-      process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-      {
-        logging: false
-      }
-    )
-    return db
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false,
   }
-}
-
-const db = createDB()
-
+)
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
@@ -33,3 +16,39 @@ module.exports = db
 if (process.env.NODE_ENV === 'test') {
   after('close database connection', () => db.close())
 }
+
+// const Sequelize = require('sequelize')
+// const pkg = require('../../package.json')
+//
+// const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+//
+// const createDB = () => {
+//   if (process.env.USER === 'notnull') {
+//     const config = require('./config')
+//     const db = new Sequelize(
+//       config.database,
+//       config.username,
+//       config.password,
+//       config.options
+//     )
+//     return db
+//   } else {
+//     const db = new Sequelize(
+//       process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//       {
+//         logging: false
+//       }
+//     )
+//     return db
+//   }
+// }
+//
+// const db = createDB()
+//
+// module.exports = db
+//
+// // This is a global Mocha hook used for resource cleanup.
+// // Otherwise, Mocha v4+ does not exit after tests.
+// if (process.env.NODE_ENV === 'test') {
+//   after('close database connection', () => db.close())
+// }
